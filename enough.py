4de2a4ba9a8f09edc2d49774654cc6f48308a0df
pantolonlubulutgr <163201119+pantolonlubulutgr@users.noreply.github.com>
2024-03-15 23:43:57 +0000

    cd Enough-Reborn pip3 install -r requirements.txt python3 enough.py

--- enough.py
+++ enough.py
@@ -24,7 +24,7 @@ while 1:
                                __/ |      
                               |___/      
     
-    Sms: {}           {}by {}@tingirifistik\n  
+    Sms: {}           {}by {}@mayyakovski\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Turbo)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçim: "))
